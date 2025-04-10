d859f1cc2291acb6255a85b17f6ad4f67e5a3159
HariHaran9209 <90740755+HariHaran9209@users.noreply.github.com>
2025-04-10 14:14:33 +0530

    Delete frontend directory

--- frontend/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React, { StrictMode } from 'react'
-import ReactDOM, { createRoot } from 'react-dom/client'
-import './css/index.css'
-import App from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-);
